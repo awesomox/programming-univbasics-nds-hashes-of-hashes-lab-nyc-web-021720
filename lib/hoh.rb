4e479f6b8ce9250591fdb047009e9d1509b16db8
tom hannaford <thom.hannaford@gmail.com>
2020-01-04 23:51:01 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,43 +3,19 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-#
-# PORTION_1 = {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class"
-#     }
-#   }
-# }
-# 
-# PORTION_2 = {
-#   label: "Order"
-# }
-# 
-# PORTION_3 = {
-#   label: "Family",
-#   sub_category: {
-#     label: "Genus",
-#   }
-# }
-# 
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
-
+PORTION_1 = {:label=>"Kingdom", :sub_category=>{:label=>"Phylum", {:sub_category=>"Class"}}}
+PORTION_2 = {:label=>"Order"}
+PORTION_3 = {:label=>"Family", {:sub_category=>"Genus"}}
+PORTION_4 = {:label=>"Species", {:sub_category=>"nil"}}
 def naming_system
-  # Remember:
-  #  Kingdom
-  #  Phylum
-  #  Class
-  #  Order
-  #  Family
-  #  Genus
-  #  Species
-  # So, if we have the "Kingdom" node we should be able to "tunnel" into the
-  # HoH all the way to Species!
-
+  naming_system = {
+    PORTION_1=> {
+      PORTION_2=> {
+        PORTION_3=> {PORTION_4
+      }}
+    }
+  }
+  p naming_system
 end
+
+naming_system
