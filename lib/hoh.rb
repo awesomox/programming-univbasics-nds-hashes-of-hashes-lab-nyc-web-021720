48411578cc2a3212e77a4d190df4bd801d6a121c
tom hannaford <thom.hannaford@gmail.com>
2020-01-04 23:57:26 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,7 +3,9 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-PORTION_1 = {:label=>"Kingdom", :sub_category=>{:label=>"Phylum", {:sub_category=>"Class"}}}
+PORTION_1 = {
+  :label=> "Kingdom", 
+  :sub_category=> "Phylum":sub_category=>{:label=>"Phylum", {:sub_category=>"Class"}}}
 PORTION_2 = {:label=>"Order"}
 PORTION_3 = {:label=>"Family", {:sub_category=>"Genus"}}
 PORTION_4 = {:label=>"Species", {:sub_category=>"nil"}}
