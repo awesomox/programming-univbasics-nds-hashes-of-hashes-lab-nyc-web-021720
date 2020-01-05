f8be2d9a8e1aea6b17a081e5ca4d0f9fd7022545
tom hannaford <thom.hannaford@gmail.com>
2020-01-05 00:06:22 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -3,18 +3,18 @@
 # that HoH as the return value for the method "naming_system."
 #
 # The tests will guide your construction.
-PORTION_1 = {
-  :sub_category=> {:label"Kingdom", 
-  :sub_category=> "Phylum":sub_category=>{:label=>"Phylum", {:sub_category=>"Class"}}}
+PORTION_1 = {:label=>"Kingdom", :sub_category=>{:label=>"Phylum", :sub_category=>{:label=>"Class"}}}
 PORTION_2 = {:label=>"Order"}
-PORTION_3 = {:label=>"Family", {:sub_category=>"Genus"}}
-PORTION_4 = {:label=>"Species", {:sub_category=>"nil"}}
+PORTION_3 = {:label=>"Family", :sub_category=>{:label=>"Genus"}}
+PORTION_4 = {:label=>"Species", :sub_category=>{}}
 def naming_system
   naming_system = {
-    PORTION_1=> {
-      PORTION_2=> {
-        PORTION_3=> {PORTION_4
-      }}
+    PORTION_1, {
+      PORTION_2, {
+        PORTION_3, {
+          PORTION_4
+        }
+      }
     }
   }
   p naming_system
